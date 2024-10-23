fc14ed12105d45f1e663397cdeacab06cddcb78c
Makis <Nazgulius@yandex.ru>
2024-10-23 11:02:39 +0300

    Update Validator.test.js

--- src/js/__tests__/Validator.test.js
+++ src/js/__tests__/Validator.test.js
@@ -1,8 +1,8 @@
 import Validator from '../Validator.js';
 
 test('Validator nickname OK', () => {
-  expect(new Validator().validateUsername('q2w2e2-2r1_23q2wer')).toBe('Подходит!');
+  expect(new Validator().validateUsername('q2w2e2-2r1_23q2wer')).toBe(true);
 })
 test('Validator nickname no OK', () => {
-  expect(new Validator().validateUsername('12356')).toBe('Мусорный.');
+  expect(new Validator().validateUsername('12356')).toBe(false);
 })
